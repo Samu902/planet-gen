e35c0850363f24bd6b35c6351aa29d9074d313a0
Samu902 <samuelemarrani@gmail.com>
2025-06-12 17:56:43 +0200

    added glsl plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
+import glsl from 'vite-plugin-glsl'
 
 // https://vite.dev/config/
 export default defineConfig({
+    plugins: [glsl()],
     server: {
         port: 3000,
     },
